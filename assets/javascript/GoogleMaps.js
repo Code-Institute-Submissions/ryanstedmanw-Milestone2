5fc32770a2869a57f9d41cc4d48509409de45af5
ryanstedmanw <ryanstedmanw@gmail.com>
2020-11-03 11:12:16 +0000

    savestate and upload

--- assets/javascript/GoogleMaps.js
+++ assets/javascript/GoogleMaps.js
@@ -4,4 +4,10 @@ var mapProp= {
   zoom:5,
 };
 var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+
+<div id="googleMap" style="width:100%;height:400px;"></div>
+<script src="./assets/javascript/GoogleMaps.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAZcqGDl30og8ov6EzPTAzhKmz3CMUvSU0&callback=myMap"></script>
+
 }
