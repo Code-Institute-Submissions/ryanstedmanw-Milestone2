1be34de50a77f6f8f79ef02661583751e7fc3f82
ryanstedmanw <ryanstedmanw@gmail.com>
2020-11-05 21:58:37 +0000

    git hub api call implimented, needs stylings.

--- assets/javascript/Github.js
+++ assets/javascript/Github.js
@@ -1,7 +1,18 @@
 $(document).ready(function(){
-    $.getJSON("https://api.github.com/users/ryanstedmanw/repos", JSON, function(result){
-    $.each(result, function(i, field){
-      $(".github").append(field + " ");
-    });
-  });
+   var request = new XMLHttpRequest()
+   request.open('get', 'https://api.github.com/users/ryanstedmanw/repos' , true)
+   request.onload = function(){
+       var data = JSON.parse(this.response);
+       var statusHTML = '';
+       $.each(data, function(i, status){
+           statusHTML += '<tr>';
+           statusHTML += '<td>' + status.id + '</td>'
+           statusHTML += '<td>' + status.name + '</td>'
+           statusHTML += '<td>' + status.html_url + '</td>'
+           statusHTML += '<td>' + status.language + '</td>'
+           statusHTML += '</tr>';
+           });
+           $('tbody').html(statusHTML);
+   }
+request.send();
 });
