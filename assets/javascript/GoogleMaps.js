34d578f768cc95e4ff8699ac44b01e73d43d9d5c
ryanstedmanw <ryanstedmanw@gmail.com>
2020-11-06 13:58:42 +0000

    portfolio page finished

--- assets/javascript/GoogleMaps.js
+++ assets/javascript/GoogleMaps.js
@@ -1,7 +1,7 @@
 function myMap() {
 var mapProp= {
-  center:new google.maps.LatLng(42,42),
-  zoom:5,
+  center:new google.maps.LatLng(50.909698, -1.404351),
+  zoom:13,
 };
 var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 
